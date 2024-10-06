f796fcf0208c9e57589fee79f65342a62dff7bd3
lunav1346 <lunav1346@kau.kr>
2024-10-07 00:28:00 +0900

    변수이름변경, 카드 입력받기

--- balatro.cpp
+++ balatro.cpp
@@ -1,3 +1,7 @@
+// 함수/변수/클래스 등 모든 이름은 대문자로 시작
+// 단어별로 대문자로 표기
+// 띄어쓰기는 언더바 이용
+
 #include <iostream>
 #include <map>
 #include <string>
@@ -7,51 +11,116 @@ using namespace std;
 
 class Poker_Hands {
 public:
-  map<string, vector<int>> Selected_Hands; // 조합 이름, 레벨, 칩, 배수 순서.
+  map<string, vector<int>> Poker_Ranks; // 조합 이름, 레벨, 칩, 배수 순서.
 
-  void initialize_pokerHand() {              // 레벨, 칩, 배수 순서
-    Selected_Hands["High_Card"] = {1, 5, 1}; // 아래 족보에 없는 카드
-    Selected_Hands["Pair"] = {1, 10, 2};     // 같은 숫자 2장 * 1쌍
-    Selected_Hands["Two_Pair"] = {1, 20, 2}; // 같은 숫자 2장 * 2쌍
-    Selected_Hands["Triple"] = {1, 30, 3};   // 같은 숫자 3장 * 1쌍
-    Selected_Hands["Straight"] = {1, 30, 4}; // 문양 상관 없이 연속 숫자 5장
-    Selected_Hands["Flush"] = {1, 35, 4}; // 숫자 상관 없이 같은 문양 5장
-    Selected_Hands["Full_House"] = {1, 40, 4}; // 트리플 + 페어
-    Selected_Hands["Four_Card"] = {1, 60, 7};  // 같은 숫자 4장 * 1쌍
-    Selected_Hands["Straight_Flush"] = {1, 100, 8}; // 같은 문양 연속 숫자 5장
+  void Initialize_PokerRank() {           // 레벨, 칩, 배수 순서
+    Poker_Ranks["High_Card"] = {1, 5, 1}; // 아래 족보에 없는 카드
+    Poker_Ranks["Pair"] = {1, 10, 2};     // 같은 숫자 2장 * 1쌍
+    Poker_Ranks["Two_Pair"] = {1, 20, 2}; // 같은 숫자 2장 * 2쌍
+    Poker_Ranks["Triple"] = {1, 30, 3};   // 같은 숫자 3장 * 1쌍
+    Poker_Ranks["Straight"] = {1, 30, 4}; // 문양 상관 없이 연속 숫자 5장
+    Poker_Ranks["Flush"] = {1, 35, 4}; // 숫자 상관 없이 같은 문양 5장
+    Poker_Ranks["Full_House"] = {1, 40, 4};      // 트리플 + 페어
+    Poker_Ranks["Four_Card"] = {1, 60, 7};       // 같은 숫자 4장 * 1쌍
+    Poker_Ranks["Straight_Flush"] = {1, 100, 8}; // 같은 문양 연속 숫자 5장
   }
-  void levelup_pokerhand() {
-    // code
+  // 족보의 레벨을 올리는 함수, [0]은 레벨, [1]은 칩, [2]는 배수
+  void LvUp_High_Card() {
+    Poker_Ranks["High_Card"][0] += 1;
+    Poker_Ranks["High_Card"][1] += 10;
+    Poker_Ranks["High_Card"][2] += 1;
+  }
+  void LvUp_Pair() {
+    Poker_Ranks["Pair"][0] += 1;
+    Poker_Ranks["Pair"][1] += 15;
+    Poker_Ranks["High_Card"][2] += 1;
+  }
+  void LvUp_Two_Pair() {
+    Poker_Ranks["Two_Pair"][0] += 1;
+    Poker_Ranks["Two_Pair"][1] += 20;
+    Poker_Ranks["Two_Pair"][2] += 1;
+  }
+  void LvUp_Triple() {
+    Poker_Ranks["Triple"][0] += 1;
+    Poker_Ranks["Triple"][1] += 20;
+    Poker_Ranks["Triple"][2] += 2;
+  }
+  void LvUp_Straight() {
+    Poker_Ranks["Straight"][0] += 1;
+    Poker_Ranks["Straight"][1] += 30;
+    Poker_Ranks["Straight"][2] += 3;
+  }
+  void LvUp_Flush() {
+    Poker_Ranks["Flush"][0] += 1;
+    Poker_Ranks["Flush"][1] += 15;
+    Poker_Ranks["Flush"][2] += 2;
+  }
+  void LvUp_Full_House() {
+    Poker_Ranks["Full_House"][0] += 1;
+    Poker_Ranks["Full_House"][1] += 25;
+    Poker_Ranks["Full_House"][2] += 2;
+  }
+  void LvUp_Four_Card() {
+    Poker_Ranks["Four_Card"][0] += 1;
+    Poker_Ranks["Four_Card"][1] += 30;
+    Poker_Ranks["Four_Card"][2] += 3;
+  }
+  void LvUp_Straight_Flush() {
+    Poker_Ranks["Straight_Flush"][0] += 1;
+    Poker_Ranks["Straight_Flush"][1] += 40;
+    Poker_Ranks["Straight_Flush"][2] += 4;
   }
 };
 
 class Trump {
 public:
-  map<string, map<string, int>> cards; // 스다하클, 숫자(AKQJ포함), 칩 순
-
-  void initialize_Trump() {
-    cards["Spade"] = {{"A", 11}, {"K", 10}, {"Q", 10}, {"J", 10}, {"10", 10},
-                      {"9", 9},  {"8", 8},  {"7", 7},  {"6", 6},  {"5", 5},
-                      {"4", 4},  {"3", 3},  {"2", 2},  {"1", 1}};
-    cards["Diamonde"] = {{"A", 11}, {"K", 10}, {"Q", 10}, {"J", 10}, {"10", 10},
-                         {"9", 9},  {"8", 8},  {"7", 7},  {"6", 6},  {"5", 5},
-                         {"4", 4},  {"3", 3},  {"2", 2},  {"1", 1}};
-    cards["Heart"] = {{"A", 11}, {"K", 10}, {"Q", 10}, {"J", 10}, {"10", 10},
-                      {"9", 9},  {"8", 8},  {"7", 7},  {"6", 6},  {"5", 5},
-                      {"4", 4},  {"3", 3},  {"2", 2},  {"1", 1}};
-    cards["Clover"] = {{"A", 11}, {"K", 10}, {"Q", 10}, {"J", 10}, {"10", 10},
-                       {"9", 9},  {"8", 8},  {"7", 7},  {"6", 6},  {"5", 5},
-                       {"4", 4},  {"3", 3},  {"2", 2},  {"1", 1}};
+  map<string, map<string, int>> Cards; // 스다하클, 숫자(AKQJ포함), 칩 순
+  vector<string> MyCards;
+  void Initialize_Trump() { // S0, D0, H0, C0은 각각 숫자 10을 의미.
+    Cards["Spade"] = {{"SA", 11}, {"SK", 10}, {"SQ", 10}, {"SJ", 10},
+                      {"S0", 10}, {"S9", 9},  {"S8", 8},  {"S7", 7},
+                      {"S6", 6},  {"S5", 5},  {"S4", 4},  {"S3", 3},
+                      {"S2", 2},  {"S1", 1}};
+    Cards["Diamond"] = {{"DA", 11}, {"DK", 10}, {"DQ", 10}, {"DJ", 10},
+                        {"D0", 10}, {"D9", 9},  {"D8", 8},  {"D7", 7},
+                        {"D6", 6},  {"D5", 5},  {"D4", 4},  {"D3", 3},
+                        {"D2", 2},  {"D1", 1}};
+    Cards["Heart"] = {{"HA", 11}, {"HK", 10}, {"HQ", 10}, {"HJ", 10},
+                      {"H0", 10}, {"H9", 9},  {"H8", 8},  {"H7", 7},
+                      {"H6", 6},  {"H5", 5},  {"H4", 4},  {"H3", 3},
+                      {"H2", 2},  {"H1", 1}};
+    Cards["Clover"] = {{"CA", 11}, {"CK", 10}, {"CQ", 10}, {"CJ", 10},
+                       {"C0", 10}, {"C9", 9},  {"C8", 8},  {"C7", 7},
+                       {"C6", 6},  {"C5", 5},  {"C4", 4},  {"C3", 3},
+                       {"C2", 2},  {"C1", 1}};
   }
 };
+
+int FindHighestScore(map<string, vector<int>> &Poker_Ranks,
+                     map<string, map<string, int>> &Cards,
+                     vector<string> &MyCards) {
+  return 0; // code
+}
 
 int main() {
   Poker_Hands pokerhands;
   Trump trump;
-  pokerhands.initialize_pokerHand();
-  trump.initialize_Trump();
-  for (const auto &pair : trump.cards["Spade"]) {
-    cout << pair.first << ": " << pair.second << endl;
+  pokerhands.Initialize_PokerRank();
+  trump.Initialize_Trump();
+
+  while (true) { // 카드 입력받고, MyCards에 push_back
+    string card;
+    cin >> card;
+    if (card == "0") {
+      break;
+    } else {
+      trump.MyCards.push_back(card);
+    }
   }
+
+  for (auto i : trump.MyCards) { // TEST 함수
+    cout << i << endl;
+  }
+
   return 0;
 }
